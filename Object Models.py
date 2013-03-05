7336eeb1e6b53e4acd4a165ea0d2223d9cfa39a2
Satrajit Ghosh <satra@mit.edu>
2013-03-05 09:33:14 -0500

    Initial commit to object models

---
+++ Object Models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# # Constructing and Querying Object Models
+# 
+# satra@mit.edu
+
+# <markdowncell>
+
+# ## Outline
+# 
+# - What is a data model?
+# - What is an object model?
+# - Using the PROV API to construct object models
+# - Querying object models from triple stores with SPARQL
+
+# <markdowncell>
+
+# ## What is a data model?
+# 
+# A data model is a conceptual formulation of information that allows 
+
